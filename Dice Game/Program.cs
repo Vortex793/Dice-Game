9d0f3b57f35e60a635b6f2f9323eb6b108aa66ca
339821175 <339821175@2260I-L29-020>
2026-09-23 13:18:34 -0400

    updated

--- Dice Game/Program.cs
+++ Dice Game/Program.cs
@@ -11,11 +11,12 @@ namespace Dice_Game
     {
         static void Main(string[] args)
         {
-            double money = 100, bet, profit, choice;
+            double money = 100, bet, profit;
             int dieTotal;
 
-            
-            Console.WriteLine("Welcome to the Dice Game!");
+            string choice;
+
+            Console.Write("Welcome to the Dice Game: ");
             Console.ReadLine();
 
             Die die1 = new Die();
@@ -25,59 +26,71 @@ namespace Dice_Game
             while (!end)
             {
                 Console.Clear();
-                Console.WriteLine("Press 1 to quit");
+                
                 Console.WriteLine("What do you want to bet for");
                 Console.WriteLine("1 - Doubles");
                 Console.WriteLine("2 - Not Doubles");
                 Console.WriteLine("3 - Even");
                 Console.WriteLine("4 - Odd");
-                choice = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("q - Quit");
 
-                //if (!double.TryParse(Console.ReadLine(), out choice) || choice <= 0 )
-                //{
-                //    Console.WriteLine("Invalid bet amount. Please enter a positive number less than or equal to your current balance.");
-                //    continue;
-                //}
 
-                Console.Write("How much do you want to bet: "); 
-                bet = Convert.ToDouble(Console.ReadLine());
+                choice = Console.ReadLine();
 
+                if (choice.ToLower() == "q")
+                {
+                    end = true;
+                    continue;
+                }
+
+                if (choice != "1" && choice != "2" && choice != "3" && choice != "4")
+                {
+                    Console.WriteLine("Invalid choice. Please enter 1, 2, 3, 4, or Q.");
+                    Console.ReadKey();
+                    continue;
+                }
+
+
+
+                Console.Write("How much do you want to bet: ");
                 if (!double.TryParse(Console.ReadLine(), out bet) || bet <= 0 || bet > money)
                 {
-                    Console.WriteLine("Invalid bet amount. Please enter a positive number less than or equal to your current balance.");
+                    Console.WriteLine("Invalid bet amount.");
                     continue;
                 }
 
                 die1.RollDie();
                 die2.RollDie();
                 dieTotal = die1.Roll + die2.Roll;
-
+                Console.Clear();
                 die1.DrawDie();
                 die2.DrawDie();
-                if (/*!double.TryParse(Console.ReadLine(), out choice) || */choice == 1 && die1.Roll == die2.Roll)  //doubles
+                if (choice == "1" && die1.Roll == die2.Roll)  //doubles
                 {
                     profit = bet * 2;
                     money += profit;
                     Console.WriteLine($"You rolled {die1.Roll} and {die2.Roll}. You win! Your new balance is ${money}");
                 }
-                else if (choice == 2 && die1.Roll != die2.Roll) //not doubles
+                else if (choice == "2" && die1.Roll != die2.Roll) //not doubles
                 {
                     profit = bet / 2;
                     money += profit;
                     Console.WriteLine($"You rolled {die1.Roll} and {die2.Roll}. You win! Your new balance is ${money}");
 
                 }
-                else if (choice == 3 && dieTotal == 2 || dieTotal == 4 || dieTotal == 6 ||  dieTotal == 8 || dieTotal == 10 || dieTotal == 12) //even
+                else if (choice == "3" && dieTotal % 2 == 0) // even
                 {
                     money += bet;
                     Console.WriteLine($"You rolled {die1.Roll} and {die2.Roll}. You win! Your new balance is ${money}");
                 }
-                else if (choice == 4 && dieTotal == 3 || dieTotal == 5 || dieTotal == 7 || dieTotal == 9 || dieTotal == 11)    //odd
+                else if (choice == "4" && dieTotal % 2 != 0) // odd
                 {
                     money += bet;
-                    die1.DrawDie();
-                    die2.DrawDie();
                     Console.WriteLine($"You rolled {die1.Roll} and {die2.Roll}. You win! Your new balance is ${money}");
+                }
+                else if (choice.ToLower() == "q")
+                {
+
                 }
                 else     //anthing else
                 {
